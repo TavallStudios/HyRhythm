acfc0f8700d6b3644cf9095bbcc99e19108673f9
TJ <tjxjnoobie@gmail.com>
2026-04-06 02:32:16 -0700

    Move hytale decompiled sources into custom-hytale-server

--- hytale-server-patch/decompiled-src/com/hypixel/hytale/protocol/ToServerPacket.java
+++
@@ -1,4 +0,0 @@
-package com.hypixel.hytale.protocol;
-
-public interface ToServerPacket extends Packet {
-}
--- hytale-server-patch/decompiled-src/com/hypixel/hytale/server/npc/role/builders/BuilderRoleAbstract.java
+++
@@ -1,7 +0,0 @@
-package com.hypixel.hytale.server.npc.role.builders;
-
-public class BuilderRoleAbstract extends BuilderRole {
-   public boolean isSpawnable() {
-      return false;
-   }
-}
